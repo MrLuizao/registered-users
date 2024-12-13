ff2472b3212227e0ae589607e754e3edaa2539f3
LuisVR <luisvr@Mac-mini-de-mac.local>
2024-12-13 00:05:35 -0600

    fix error load data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,16 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'http://localhost:4200'
+  apiUrl: 'http://localhost:4200',
+
+  firebase: {
+    apiKey: "AIzaSyBQ0S68gorkbFP-eBZsCRraZbrp7YV4eu4",
+    authDomain: "form-qr.firebaseapp.com",
+    projectId: "form-qr",
+    storageBucket: "form-qr.firebasestorage.app",
+    messagingSenderId: "274695449880",
+    appId: "1:274695449880:web:8aa249019e4ea7dc4c026d",
+    measurementId: "G-E70S83NLW2"
+  }
+  
 };
